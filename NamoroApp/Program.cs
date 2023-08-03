4d2ec58b8f9aac269e1f54bb7434b8b0667b51d4
Everton Leão <everleao@gmail.com>
2023-08-02 23:01:14 -0300

    api

--- NamoroApp/Program.cs
+++ NamoroApp/Program.cs
@@ -41,7 +41,6 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
